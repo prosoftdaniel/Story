d0fa21f54ede4e88c90be85ad052c716454781f9
Daniel Burnar <prosoftdaniel@gmail.com>
2021-10-03 13:48:03 +0300

    modificare 2

---
+++ udderGitTest/git token.swift
@@ -0,0 +1,11 @@
+//
+//  git token.swift
+//  udderGitTest
+//
+//  Created by BURNAR DANIEL on 03.10.2021.
+//
+
+import Foundation
+struct git_token {
+    static let token = "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"
+}

c6c469d0577d0ee581a9dbe0925739d6c3885d7b
Daniel Burnar <prosoftdaniel@gmail.com>
2021-10-03 13:15:24 +0300

    Initial Commit

---
+++ udderGitTest/udderGitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  udderGitTestApp.swift
+//  udderGitTest
+//
+//  Created by BURNAR DANIEL on 03.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct udderGitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
